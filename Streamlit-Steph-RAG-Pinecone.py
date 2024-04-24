a371d8333dfd7bcec200b351ecb194da4cc6d3ee
justinrothaug <94494722+justinrothaug@users.noreply.github.com>
2024-04-24 13:29:35 -0700

    Update Streamlit-Steph-RAG-Pinecone.py

--- Streamlit-Steph-RAG-Pinecone.py
+++ Streamlit-Steph-RAG-Pinecone.py
@@ -53,7 +53,7 @@ client2= ElevenLabs(api_key= os.environ["ELEVEN_LABS_API_KEY"])
 
 #Set up the Environment
 st.set_page_config(page_title="Steph Curry")
-assistant_logo = 'https://www.basketballemotion.com/imagesarticulos/216257/grandes/balon-wilson-nba-player-icon-mini-stephen-curry-blue-yellow-silver-4.webp'
+assistant_logo = 'https://res.cloudinary.com/teepublic/image/private/s--hmN6y7X_--/c_crop,x_10,y_10/c_fit,h_1109/c_crop,g_north_west,h_1260,w_1008,x_-84,y_-76/co_rgb:36538b,e_colorize,u_Misc:One%20Pixel%20Gray/c_scale,g_north_west,h_1260,w_1008/fl_layer_apply,g_north_west,x_-84,y_-76/bo_0px_solid_white/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:53/co_ffffff,e_outline:inner_fill:53/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1544500888/production/designs/3704748_0.jpg'
 
 #Set up the Video
 video_html = """
